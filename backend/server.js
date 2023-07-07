e188aecf5e9424492413a474cbd29cef03e77d3e
edidiesky <essienedidiong1000@gmail.com>
2023-07-07 12:22:06 -0700

    new

--- backend/server.js
+++ backend/server.js
@@ -63,17 +63,17 @@ mongoose.connect(
 
 // production mode process
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running....");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 // Middlewares
 app.use(NotFound);
